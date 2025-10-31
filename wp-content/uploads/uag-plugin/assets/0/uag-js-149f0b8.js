9417366e6bb0fa1b38a7b3eac18b2732ac275dd4
Luis Alejandro <luisalejandro.ramonr@gmail.com>
2025-10-31 11:04:37 -0600

    Initial commit

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-149f0b8.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-265e6931' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-c20461b3' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-d1b8f293' );
+});
+ });
---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-9291cd.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-7gznj7j0' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-fagaho7s' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-9dj7ipaf' );
+});
+ });
